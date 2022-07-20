cec356298d6c259fadc87dd5af18a4dc8ad79e39
Gustavo Inacio <gutiinacio@gmail.com>
2022-07-20 10:02:32 -0300

    testing if default routes can differentiate between then by their http methods, and removing sysouts, code got a little more clean

---
+++ src/main/java/util/Exceptions/UnexpectedExceptionOnCreatingActionClass.java
@@ -0,0 +1,11 @@
+package util.Exceptions;
+
+public class UnexpectedExceptionOnCreatingActionClass extends Exception{
+
+	private static final long serialVersionUID = 1L;
+	
+	
+	public UnexpectedExceptionOnCreatingActionClass(String msg) {
+		super(msg);
+	}
+}
